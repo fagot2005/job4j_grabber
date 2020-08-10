d14089d202b5213d20747dd21d969d3cb3d882c3
fagot2005 <60795549+fagot2005@users.noreply.github.com>
2020-08-10 12:57:43 +0300

    1. Quartz [#313727]

--- src/main/java/quartz/AlertRabbit.java
+++ src/main/java/quartz/AlertRabbit.java
@@ -2,6 +2,11 @@ package quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import static org.quartz.JobBuilder.*;
@@ -10,6 +15,7 @@ import static org.quartz.TriggerBuilder.*;
 
 public class AlertRabbit {
     public static void main(String[] args) {
+        int interval = readProperties("./src/main/resources/rabbit.properties");
         try {
             List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
@@ -18,7 +24,7 @@ public class AlertRabbit {
             data.put("store", store);
             JobDetail job = newJob(Rabbit.class).usingJobData(data).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(10)
+                    .withIntervalInSeconds(interval)
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -28,9 +34,26 @@ public class AlertRabbit {
             Thread.sleep(5000);
             scheduler.shutdown();
             System.out.println(store);
-        } catch (SchedulerException | InterruptedException se) {
+        } catch (SchedulerException | InterruptedException | FileNotFoundException se) {
             se.printStackTrace();
         }
+    }
+
+    private static int readProperties(String patch) {
+       int resalt = 0;
+        String[] temp;
+        try (BufferedReader in = new BufferedReader(new FileReader("./src/main/resources/rabbit.properties"))) {
+            do {
+                temp = in.readLine().split("=");
+                if (temp[0].equals("rabbit.interval")) {
+                    resalt = Integer.parseInt(temp[1]);
+                }
+            } while (temp.length != 0);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } return resalt;
     }
 
     public static class Rabbit implements Job {
