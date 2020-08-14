a70e72c5677c416d8e1df6d52ef58aeb57817351
fagot2005@gmail.com <v.lashchenko.1976@gmail.com>
2020-08-14 23:37:49 +0300

    1.1. Job c параметрами [#313731]

--- src/main/java/quartz/AlertRabbit.java
+++ src/main/java/quartz/AlertRabbit.java
@@ -2,10 +2,10 @@ package quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
@@ -13,9 +13,14 @@ import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
+    private Connection cn;
+    private static final String GREATE_TABLE = "create table rabbit (id serial primary key, data date);";
+    private static final String INSERT_DATE = "insert into rabbit values (1, CURRENT_DATE);";
+
     public static void main(String[] args) {
         AlertRabbit alertRabbit = new AlertRabbit();
         int interval = alertRabbit.readProperties("./src/main/resources/rabbit.properties");
+        alertRabbit.initConnection();
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
@@ -56,5 +61,20 @@ public class AlertRabbit {
         } catch (IOException e) {
             e.printStackTrace();
         } return resalt;
+    }
+
+    private void initConnection() {
+        try (InputStream in = Rabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            cn = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
