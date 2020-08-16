d174db38e4dd5cbf7e771821156e8115b8778daa
fagot2005@gmail.com <v.lashchenko.1976@gmail.com>
2020-08-16 14:38:04 +0300

    2.2. Модель данных - Post. [#313734]

--- src/main/java/quartz/Post.java
+++ src/main/java/quartz/Post.java
@@ -3,37 +3,57 @@ package quartz;
 import java.time.LocalDate;
 
 public class Post {
-    private String idPost;
-    private String body;
-    private LocalDate datePost;
+    private String id;
+    private String name;
+    private String text;
+    private String link;
+    private LocalDate created;
 
-    public Post(String idPost, String body, LocalDate datePost) {
-        this.idPost = idPost;
-        this.body = body;
-        this.datePost = datePost;
+    public Post(String id, String name, String text, String link, LocalDate created) {
+        this.id = id;
+        this.name = name;
+        this.text = text;
+        this.link = link;
+        this.created = created;
     }
 
-    public String getIdPost() {
-        return idPost;
+    public String getId() {
+        return id;
     }
 
-    public void setIdPost(String idPost) {
-        this.idPost = idPost;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public String getBody() {
-        return body;
+    public String getName() {
+        return name;
     }
 
-    public void setBody(String body) {
-        this.body = body;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public LocalDate getDatePost() {
-        return datePost;
+    public String getText() {
+        return text;
     }
 
-    public void setDatePost(LocalDate datePost) {
-        this.datePost = datePost;
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDate created) {
+        this.created = created;
     }
 }
