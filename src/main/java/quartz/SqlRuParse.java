1f220c22c6dc1479f24362a04160c228dc83b00c
fagot2005@gmail.com <v.lashchenko.1976@gmail.com>
2020-08-18 13:50:59 +0300

    2.1. Преобразование даты [#313737]

--- src/main/java/quartz/SqlRuParse.java
+++ src/main/java/quartz/SqlRuParse.java
@@ -7,8 +7,6 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,60 +46,7 @@ public class SqlRuParse implements Parse {
         String id = String.valueOf(posts.size() + 1);
         Document doc = Jsoup.connect(link).get();
         String text= doc.select(".msgBody").get(index).text();
-        Post post = new Post(id, link, description, text, transformData(datePost));
+        Post post = new Post(id, link, description, text, new ParsLocalData().transformData(datePost));
         return post;
-    }
-
-    public LocalDate transformData(String dataOfTransform) throws ParseException {
-        String today = "сегодня";
-        String yesterday = "вчера";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
-        LocalDate localDate;
-        String localDataCeng;
-        String[] dataArray = dataOfTransform.split(",");
-        if (dataArray[0].equals(today)) {
-            localDate = LocalDate.now();
-        } else if (dataArray[0].equals(yesterday)) {
-            localDate = LocalDate.now().minusDays(1);
-        } else {
-            String[] localDataArray = dataArray[0].split(" ");
-            localDataCeng = "";
-            if (localDataArray[0].length() == 1) {
-                localDataArray[0] = "0" + localDataArray[0];
-            }
-            if (localDataArray[1].equals("янв")) {
-                localDataCeng = parsMonth(localDataArray, "01");
-            } else if (localDataArray[1].equals("фев")) {
-                localDataCeng = parsMonth(localDataArray, "02");
-            } else if (localDataArray[1].equals("мар")) {
-                localDataCeng = parsMonth(localDataArray, "03");
-            } else if (localDataArray[1].equals("апр")) {
-                localDataCeng = parsMonth(localDataArray, "04");
-            } else if (localDataArray[1].equals("май")) {
-                localDataCeng = parsMonth(localDataArray, "05");
-            } else if (localDataArray[1].equals("июн")) {
-                localDataCeng = parsMonth(localDataArray, "06");
-            } else if (localDataArray[1].equals("июл")) {
-                localDataCeng = parsMonth(localDataArray, "07");
-            } else if (localDataArray[1].equals("авг")) {
-                localDataCeng = parsMonth(localDataArray, "08");
-            } else if (localDataArray[1].equals("сен")) {
-                localDataCeng = parsMonth(localDataArray, "09");
-            } else if (localDataArray[1].equals("окт")) {
-                localDataCeng = parsMonth(localDataArray, "10");
-            } else if (localDataArray[1].equals("ноя")) {
-                localDataCeng = parsMonth(localDataArray, "11");
-            } else if (localDataArray[1].equals("дек")) {
-                localDataCeng = parsMonth(localDataArray, "08");
-            }
-            localDate = LocalDate.parse(localDataCeng, formatter);
-        }
-        return localDate;
-    }
-
-    private String parsMonth(String[] localDataArray, String s) {
-        String localDataCeng;
-        localDataCeng = localDataArray[0] + "/" + s + "/" + localDataArray[2];
-        return localDataCeng;
     }
 }
---
+++ src/test/java/quartz/ParsLocalDataTest.java
@@ -0,0 +1,39 @@
+package quartz;
+
+import org.hamcrest.core.IsNull;
+import org.junit.Test;
+import java.text.ParseException;
+import java.time.LocalDate;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ParsLocalDataTest {
+    @Test
+    public void whenTodayLocalData() throws ParseException {
+        ParsLocalData parsLocalData = new ParsLocalData();
+        assertThat(parsLocalData.transformData("сегодня, 11:07"),
+                is(LocalDate.now()));
+    }
+
+    @Test
+    public void whenYesterdayLocalData() throws ParseException {
+        ParsLocalData parsLocalData = new ParsLocalData();
+        assertThat(parsLocalData.transformData("вчера, 11:07"),
+                is(LocalDate.now().minusDays(1)));
+    }
+
+    @Test
+    public void whenEmptyLocalData() throws ParseException {
+        ParsLocalData parsLocalData = new ParsLocalData();
+        assertThat(parsLocalData.transformData(""),
+                IsNull.nullValue());
+    }
+
+    @Test
+    public void whenUsualLocalData() throws ParseException {
+        ParsLocalData parsLocalData = new ParsLocalData();
+        assertThat(parsLocalData.transformData("29 июл 20, 15:09"),
+                is(LocalDate.of(2020, 7, 29)));
+    }
+
+}
